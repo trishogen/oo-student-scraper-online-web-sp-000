626c43ad6bba4d3c221d2a132828d6aee5743cf8
Trish Ogen <patriciaogen@gmail.com>
2019-06-13 00:34:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,32 @@ require 'pry'
 
 class Scraper
 
+  @@all = []
+
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css(".student-card").each do |student|
+    student_hash = {
+      name: student.css("h4.student-name").text,
+      location: student.css("p.student-location").text,
+      profile_url: student.css("a").attribute("href").value
+    }
+    @@all << student_hash
+    end
+    @@all
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    profile = Nokogiri::HTML(open(profile_url))
+    student_hash = {
+      twitter: profile.css("a")[1].attribute("href").value,
+      linkedin: profile.css("a")[2].attribute("href").value,
+      github: profile.css("a")[3].attribute("href").value,
+      blog: profile.css("a")[4].attribute("href").value,
+      profile_quote: profile.css("div.profile-quote").text,
+      bio: profile.css("p").text.gsub("\n", ' ').squeeze(' ')
+    }
+    student_hash.reject { |k, v| v.nil? }
   end
 
 end
-
